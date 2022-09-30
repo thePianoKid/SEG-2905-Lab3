4432efc01ae4ebe0aa9d644025e35d5bbc9280f2
xbariq <91578484+xbariq@users.noreply.github.com>
2022-09-30 18:34:22 -0400

    Create Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,9 @@
 /* 
 SECTION 1 */
-import android.os.Bundle;
-import androidview.Menu;
-import android.app.Activity;
-import android.view.View;
-import android.view.Menu;
-import android.widget.EditText;
 
-public class MainActivity extends Activity {
 
-	private enum Operatoer {none,add, minus, multiply, divide}
-	private double data1 = 0, data2 = 0;
-	private Operator optr = Operator.none;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu (Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	public void btn00ClickView view) {
-		EditText eText = (EditText)findViewById(R.id.resultEdit);
-		eText.setText(eText.getText()+"0");
-	}
 
 /* 
 SECTION 2 */
@@ -39,13 +13,66 @@ SECTION 2 */
 
 /* 
 SECTION 3 */
+public void btn01Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText ()+"1') ;
+}
 
+public void btn02Click (View view) {
+    EditText Text = (EditText) findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"2");   
+} 
+
+public void btn03Click (View view) { 
+    EditText eText = (EditText) findViewById (R.id. resultEdit);
+    eText.setText(eText.getText()+"3");
+}
+
+public void btn04Click (View view){ 
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"4');
+} 
+public void btn05Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"5");
+}
+public void btn06Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText () +"6") ;
+} 
 
 
 
 /* 
 SECTION 4 */
+public void btn07Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText () +"7") ;
+}
+public void btn08Click (View view) { 
+    EditText eText = (EditText) findViewById (R.id. resultEdit);
+    eText.setText (eText.getText () +"8") ;
+}
+public void btn09Click (View view) {
+    EditText eText= (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText () +"9") ;
+}
+public void btnAddClick (View view) {
+    optr = Operator. add;
+    EditText eText=(EditText)findViewById(R.id.resultEdit);
+    detal = = Double.parseDouble(eText.getText () .toString ());
+    eText.setText("");
 
+}
+public void btnMinusClick (View view){
+    optr = Operator.minus;
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    datal = Double.parseDouble(eText.getText () .toString ()) :
+    eText.setText("");
+}
+
+
+          
 
 
 /* 
