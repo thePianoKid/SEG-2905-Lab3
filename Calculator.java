1af83e3fdfd4d4355056e28ff2dccab22df08a9b
xbariq <91578484+xbariq@users.noreply.github.com>
2022-09-30 19:03:40 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -62,22 +62,22 @@ public void btn06Click(View view) {
 SECTION 3 */
 public void btn01Click (View view) {
     EditText eText = (EditText)findViewById(R.id.resultEdit);
-    eText.setText (eText.getText ()+"1') ;
+    eText.setText (eText.getText ()+"1") ;
 }
 
 public void btn02Click (View view) {
-    EditText Text = (EditText) findViewById(R.id.resultEdit);
+    EditText Text = (EditText)findViewById(R.id.resultEdit);
     eText.setText(eText.getText()+"2");   
 } 
 
 public void btn03Click (View view) { 
-    EditText eText = (EditText) findViewById (R.id. resultEdit);
+    EditText eText = (EditText)findViewById(R.id. resultEdit);
     eText.setText(eText.getText()+"3");
 }
 
 public void btn04Click (View view){ 
     EditText eText = (EditText)findViewById(R.id.resultEdit);
-    eText.setText(eText.getText()+"4');
+    eText.setText(eText.getText()+"4");
 } 
 public void btn05Click (View view) {
     EditText eText = (EditText)findViewById(R.id.resultEdit);
@@ -98,7 +98,7 @@ public void btn07Click (View view) {
     eText.setText (eText.getText () +"7") ;
 }
 public void btn08Click (View view) { 
-    EditText eText = (EditText) findViewById (R.id. resultEdit);
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
     eText.setText (eText.getText () +"8") ;
 }
 public void btn09Click (View view) {
@@ -108,14 +108,14 @@ public void btn09Click (View view) {
 public void btnAddClick (View view) {
     optr = Operator. add;
     EditText eText=(EditText)findViewById(R.id.resultEdit);
-    detal = = Double.parseDouble(eText.getText () .toString ());
+    detal = = Double.parseDouble(eText.getText().toString ());
     eText.setText("");
 
 }
 public void btnMinusClick (View view){
     optr = Operator.minus;
     EditText eText = (EditText)findViewById(R.id.resultEdit);
-    datal = Double.parseDouble(eText.getText () .toString ()) :
+    datal = Double.parseDouble(eText.getText().toString ()) :
     eText.setText("");
 }
 
