d9fe66bb159bfc399a61a809db0beec777e2acbd
xbariq <91578484+xbariq@users.noreply.github.com>
2022-09-30 18:36:52 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,33 @@ SECTION 2 */
 
 /* 
 SECTION 3 */
+public void btn01Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText ()+"1') ;
+}
+
+public void btn02Click (View view) {
+    EditText Text = (EditText) findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"2");   
+} 
+
+public void btn03Click (View view) { 
+    EditText eText = (EditText) findViewById (R.id. resultEdit);
+    eText.setText(eText.getText()+"3");
+}
+
+public void btn04Click (View view){ 
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"4');
+} 
+public void btn05Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText(eText.getText()+"5");
+}
+public void btn06Click (View view) {
+    EditText eText = (EditText)findViewById(R.id.resultEdit);
+    eText.setText (eText.getText () +"6") ;
+} 
 
 
 
