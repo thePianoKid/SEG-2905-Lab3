e186671f278c3253f7446ffe41d17ce92e787dac
AndresA-S <102438454+AndresA-S@users.noreply.github.com>
2022-09-30 18:39:41 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,35 @@
 /* 
 SECTION 1 */
+import android.os.Bundle;
+import androidview.Menu;
+import android.app.Activity;
+import android.view.View;
+import android.view.Menu;
+import android.widget.EditText;
 
+public class MainActivity extends Activity {
+
+	private enum Operatoer {none,add, minus, multiply, divide}
+	private double data1 = 0, data2 = 0;
+	private Operator optr = Operator.none;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu (Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void btn00ClickView view) {
+		EditText eText = (EditText)findViewById(R.id.resultEdit);
+		eText.setText(eText.getText()+"0");
+	}
 
 
 
